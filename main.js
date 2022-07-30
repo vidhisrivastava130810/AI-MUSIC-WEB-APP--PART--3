0995736b28cf78fcfa7c1c011954770415ffec6e
vidhisrivastava130810 <107047210+vidhisrivastava130810@users.noreply.github.com>
2022-07-30 17:56:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+song1_status = "";
+song2_status = "";
 
 function preload(){
     song1 = loadSound("music.mp3");
@@ -23,6 +26,23 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX , leftWristY , 20);
+        song2.stop;
+        if(song1_status == false)
+        {
+            song1.play();
+            document.getElementById("song").innerHTML = "Playing Song 1";
+        }
+    }
 }
 
 function modelLoded(){
@@ -33,6 +53,9 @@ function gotPoses(results){
     if(results.length > 0);
 {
     console.log(results);
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("Score Left Wrist = "+scoreLeftWrist);
+
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
     console.log("Left Wrist X = " +leftWristX+"Left Wrist Y = "+leftWristY);
